9bda76669393539b11d128beb0fa7f561d4a4fa0
henkrs1 <eahenke@gmail.com>
2015-11-08 22:42:55 -0600

    altered temp filter to hide expression when no temp set

--- js/components/weather/weather_filters.js
+++ js/components/weather/weather_filters.js
@@ -4,10 +4,16 @@
     //Takes a tempertature in fahrenheit and formats it, followed by Celsius equivalent
     weatherApp.filter('temperature', function() {
         return function(temp) {
-                temp = parseInt(temp);
-                var celsius = Math.round((temp - 32) * ( 5/ 9)); 
-                var output = temp + '\xB0F / ' + celsius + '\xB0C';
-                return output;
+                if(temp) {
+                    temp = parseInt(temp);
+                    var celsius = Math.round((temp - 32) * ( 5/ 9)); 
+                    var output = temp + '\xB0F / ' + celsius + '\xB0C';
+                    return output;
+                    
+                } else {
+                    //prevent showing 'NaN' if null or undefined
+                    return '';
+                }
         };
     });
 })(window);
